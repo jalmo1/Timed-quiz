253ec1aeda8313cda4272c9e467f1b08b07ca715
Joshua Almodovar <joshuarosamktp4@gmail.com>
2021-05-21 22:35:29 -0400

    created question arrays

---
+++ assets/js/quiz.js
@@ -0,0 +1,28 @@
+var questions = [
+  {
+    question: "1. What engine has the most cylinders?",
+    choices: ["V8", "I4", "I6", "V4"],
+    correctAnswer: 0,
+  },
+  {
+    question: "2. What engine is found inside a Toyota Supra?",
+    choices: ["LS2", "RB26", "2JZ", "K24"],
+    correctAnswer: 2,
+  },
+  {
+    question: "3. How many cylinders does a V8 have?",
+    choices: ["5", "6", "8", "4"],
+    correctAnswer: 2,
+  },
+  {
+    question: "4. What type of race goes in a straight line?",
+    choices: ["Circuit racing", "Time attack", "Drag racing", "Drifting"],
+    correctAnswer: 2,
+  },
+  {
+    question: "5. When should you get an oil change?",
+    choices: ["5,000 miles", "2,000 miles", "10,000 miles", "100 miles"],
+    correctAnswer: 0,
+  },
+];
+console.log(questions);
